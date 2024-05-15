84cf533af07b6d1350147e5a40ca3e78ff83b054
Akinwumidi <israelakin14@gmail.com>
2024-05-16 00:12:32 +0100

    core update: Added Dependency Injection for DbContext

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using passbook_core_mvc_crud_app.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Dependency Injection (DI) for DbContext
+builder.Services.AddDbContext<TransactionDbContext>(options=>options.UseSqlServer());
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
