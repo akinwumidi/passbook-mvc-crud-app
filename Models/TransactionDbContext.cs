85d385f5e7e47540e029e08c9d3d27930766c66f
Akinwumidi <israelakin14@gmail.com>
2024-05-15 12:07:49 +0100

    core update: Added Transaction.cs Model

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace passbook_core_mvc_crud_app.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) :base(options
+            )
+        {
+            
+        }
+    }
+}
---
+++ Models/Transactions.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace passbook_core_mvc_crud_app.Models
+{
+    public class Transactions
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        public String AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public String BeneficiaryName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+
+        public String BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+
+        public String SWIFTCode { get; set; }
+        public int Amount { get; set; }
+        public DateTime Date {  get; set; }
+    }
+}
