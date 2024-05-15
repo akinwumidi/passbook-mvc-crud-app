3e7b1d2845aa18c008da0b504f0364fcd77bc341
Akinwumidi <israelakin14@gmail.com>
2024-05-15 23:54:17 +0100

    core: complete TransactionDbContext

--- Models/TransactionDbContext.cs
+++ Models/TransactionDbContext.cs
@@ -4,10 +4,11 @@ namespace passbook_core_mvc_crud_app.Models
 {
     public class TransactionDbContext:DbContext
     {
-        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) :base(options
-            )
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) :base(options)
         {
             
         }
+
+        public DbSet<Transactions> Transactions { get; set; }
     }
 }
